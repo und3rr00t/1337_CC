e253e6501e3177e76b6d82da1bc996d010c5fd0b
S0k3t R3ap3r <118362464+und3rr00t@users.noreply.github.com>
2025-10-23 19:51:45 +0100

    Add files via upload

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oused-da <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 21:36:25 by oused-da          #+#    #+#             */
+/*   Updated: 2025/10/21 21:48:50 by oused-da         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*next;
+	
+	if (!lst || !del)
+		return ;
+	while (*lst != NULL)
+	{
+		del((*lst)->content);
+		next = (*lst)->next;
+		free(*lst);
+		(*lst) = next;
+	}
+}
