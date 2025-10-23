35c8a68d3608873089eefcec0a627c0db3e20070
und3rr00t <ousamaeddahbir@gmail.com>
2025-10-23 12:30:26 +0100

    push

--- libft/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int main()
-{
-	char d[15];
-	printf("%zu\n", ft_strlcpy(d, "012345678901234567890123456789", 20));
-	printf("%s", d);
-}
