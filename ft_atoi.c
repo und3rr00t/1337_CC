f7ba0af3db074360cefd23e3fc054c8014fdbc5e
S0k3t R3ap3r <118362464+und3rr00t@users.noreply.github.com>
2025-10-25 13:38:20 +0100

    Comment out
    Commented out the ft_atoi function implementation.

--- ft_atoi.c
+++ ft_atoi.c
@@ -36,3 +36,34 @@ int	ft_atoi(const char *nptr)
 	}
 	return (o * s);
 }
+
+/*
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result < 0 && sign == -1)
+			return (0);
+		else if (result < 0)
+			return (-1);
+		i++;
+	}
+	return (result * sign);
+} */
