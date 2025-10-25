7fab7b7720ec07a889c34bfc607022123cda9a2d
S0k3t R3ap3r <118362464+und3rr00t@users.noreply.github.com>
2025-10-25 14:04:56 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -32,38 +32,11 @@ int	ft_atoi(const char *nptr)
 	while (ft_isdigit(nptr[i]))
 	{
 		o = o * 10 + (nptr[i] - 48);
+		if (o < 0 && s = -1)
+			return (0);
+		else if (o < 0)
+			return (-1);
 		i++;
 	}
 	return (o * s);
 }
-
-/*
-
-int	ft_atoi(const char *str)
-{
-	int		i;
-	int		sign;
-	long	result;
-
-	i = 0;
-	result = 0;
-	sign = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		if (result < 0 && sign == -1)
-			return (0);
-		else if (result < 0)
-			return (-1);
-		i++;
-	}
-	return (result * sign);
-} */
