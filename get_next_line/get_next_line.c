307cc380868e75c78b07d9d8bf8535154050e01f
S0k3t R3ap3r <118362464+und3rr00t@users.noreply.github.com>
2025-11-26 17:54:19 +0100

    Refactor

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,71 +1,145 @@
-#include "get_next_line.h"
-
-static char	*extract(char *s)
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 42
+size_t ft_strlen(const char *s)
 {
-	int		i = 0;
-	int		len;
-	char	*p;
+    size_t  i;
+    i = 0;
 
-	if (!s || !s[0])
-		return (NULL);
-	while (s[i] && s[i] != '\n')
-		i++;
-	len = i + (s[i] == '\n');
-	p = malloc(len + 1);
-	if (!p)
-		return (NULL);
-	i = 0;
-	while (s[i] && s[i] != '\n')
-	{
-		p[i] = s[i];
-		i++;
-	}
-	if (s[i] == '\n')
-		p[i++] = '\n';
-	p[i] = 0;
-	return (p);
+    if (!s)
+        return (0);
+    while (s[i])
+        i++;
+    return (i);
+}
+char *ft_strchr(const char *s, int c)
+{
+    if (!s)
+        return (NULL);
+    int i;
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            return ((char *)&s[i]);
+        i++;
+    }
+    return (NULL);
+}
+char *ft_strjoin(char *s1, char *s2)
+{
+    if (!s1)
+    {
+        s1 = malloc(1);
+        s1[0] = 0;
+    }
+    char *joined;
+    size_t s1_len = ft_strlen(s1);
+    size_t s2_len = ft_strlen(s2);
+    joined = malloc(s1_len + s2_len + 1);
+    if (!joined)
+    {
+        free(s1);
+        return (NULL);
+    }
+    int i = 0;
+    while (s1[i])
+    {
+        joined[i] = s1[i];
+        i++;
+    }
+    int j = 0;
+    while (s2[j])
+        joined[i++] = s2[j++];
+    joined[i] = 0;
+    free(s1);
+    return (joined);
+}
+char    *read_file(int fd, char *stash)
+{
+    char *buffer;
+    int readed_bytes;
+    
+    buffer = malloc(BUFFER_SIZE + 1);
+    if (!buffer)
+        return (NULL);
+    readed_bytes = 1;
+    while (!ft_strchr(stash, '\n') && readed_bytes != 0)
+    {
+        readed_bytes = read(fd, buffer, BUFFER_SIZE);
+        if (readed_bytes == -1)
+        {
+            free(buffer);
+            free(stash);
+            return (NULL);
+        }
+        buffer[readed_bytes] = 0;
+        stash = ft_strjoin(stash, buffer);
+    }
+    free(buffer);
+    return (stash);
+}
+char *ft_line(char *stash)
+{
+    char *line;
+    if (!stash || !stash)
+        return (NULL);
+    int i = 0;
+    while (stash[i] && stash[i] != '\n')
+        i++;
+    if (stash[i] == '\n')
+        line = malloc(i + 2);
+    else
+        line  = malloc(i + 1);
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (stash[i] && stash[i] != '\n')
+    {
+        line[i] = stash[i];
+        i++;
+    }
+    if (stash[i] == '\n')
+        line[i++] = '\n';
+    line[i] = 0;
+    return (line);
+}
+char *ft_new_stash(char *stash)
+{
+    char *new_stash;
+    int i = 0;
+    int j = 0;
+    while (stash[i] && stash[i] != '\n')
+        i++;
+    if (!stash[i])
+    {
+        free(stash);
+        return (NULL);
+    }
+    i++;
+    new_stash = malloc(ft_strlen(stash) - i + 1);
+    if (!new_stash)
+        return(free(stash), NULL);
+    while (stash[i])
+        new_stash[j++] = stash[i++];
+    new_stash[j] = 0;
+    free(stash);
+    return(new_stash);
+}
+char *get_next_line(int fd)
+{
+    static char *stash;
+    char    *line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    stash = read_file(fd, stash);
+    if (!stash)
+        return (NULL);
+    line = ft_line(stash);
+    stash = ft_new_stash(stash);
+    return (line);
 }
 
-static char	*clean(char *s)
-{
-	int		i = 0;
-	int		j = 0;
-	char	*p;
-
-	while (s[i] && s[i] != '\n')
-		i++;
-	if (!s[i])
-		return (free(s), NULL);
-	i++;
-	p = malloc(ft_strlen(s) - i + 1);
-	if (!p)
-		return (NULL);
-	while (s[i])
-		p[j++] = s[i++];
-	p[j] = 0;
-	free(s);
-	return (p);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*s;
-	char		b[BUFFER_SIZE + 1];
-	int			x;
-	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	x = 1;
-	while (!ft_strchr(s, '\n') && x > 0)
-	{
-		x = read(fd, b, BUFFER_SIZE);
-		if (x < 0)
-			return (free(s), s = NULL, NULL);
-		b[x] = 0;
-		s = ft_strjoin(s, b);
-	}
-	line = extract(s);
-	s = clean(s);
-	return (line);
-}
