015c3b0322a16cc513381e5ca9f681750a225d2c
Ousama Ed-Dahbi <oused-da@e3r10p3.1337.ma>
2025-11-26 20:57:04 +0100

    GNl

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -31,27 +31,28 @@ char	*ft_strchr(const char *s, int c)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*joined;
-	size_t	i;
-	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
+	int		i;
+	int		j;
 
 	if (!s1)
 	{
-		s1 = (char *)malloc(1 * sizeof(char));
-		s1[0] = '\0';
+		s1 = malloc(1);
+		s1[0] = 0;
 	}
-	if (!s1 || !s2)
-		return (NULL);
-	joined = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	joined = malloc(s1_len + s2_len + 1);
 	if (!joined)
 		return (free(s1), NULL);
-	i = -1;
+	i = 0;
+	while (s1[i])
+		joined[i] = s1[i++];
 	j = 0;
-	if (s1)
-		while (s1[++i] != '\0')
-			joined[i] = s1[i];
-	while (s2[j] != '\0')
+	while (s2[j])
 		joined[i++] = s2[j++];
-	joined[i] = '\0';
+	joined[i] = 0;
 	free(s1);
 	return (joined);
 }
